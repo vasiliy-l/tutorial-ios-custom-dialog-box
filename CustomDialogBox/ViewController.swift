ce743812812d8efce5a25ff828dc38759aeabb93
Vasiliy Lada <vasiliy.lada@protonmail.com>
2019-05-01 17:03:27 +0300

    Initial Commit

---
+++ CustomDialogBox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomDialogBox
+//
+//  Created by  William on 5/1/19.
+//  Copyright © 2019 Vasiliy Lada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
